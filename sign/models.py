d519b2d40d98bde83287a5092a2cb5851ab1600b
tinfeng <624783279@qq.com>
2019-06-14 17:59:27 +0800

    继续学习

--- sign/models.py
+++ sign/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    limit = models.IntegerField()
+    status = models.BooleanField()
+    address = models.CharField(max_length=200)
+    start_time = models.DateField('evens time')
+    create_time = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Guest(models.Model):
+    event = models.ForeignKey(Event)
+    realname = models.CharField(max_length=64)
+    phone = models.CharField(max_length=16)
+    email = models.EmailField()
+    sign = models.BooleanField()
+    create_time = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.realname
+
+
+class Meta:
+    unique_together = ("event", "phone")
